7f19bd7d12e89be7c660d069df4eeaf9be1e744d
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2021-11-29 18:33:06 +0530

    pattern four

---
+++ pattern_four.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class pattern_four {
+    public static void main(String[] args) {
+        Scanner src = new Scanner(System.in);
+        int n= src.nextInt();
+        int i=1;
+        int count=1;
+        while(i<=n){
+            int j=1;
+            while(j<=n){
+                System.out.print(" " +count);
+                count=count+1;
+                j=j+1;
+            }
+            System.out.println("  ");
+            i=i+1;
+        }
+
+    }
+}
+
