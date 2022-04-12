03ebda8329b2e4f039208fbfd26669006ee491e7
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-04-12 23:25:23 +0530

    sum odd num

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= 2 * n; i++) {
+            if (i % 2 == 0) {
+                continue;
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
