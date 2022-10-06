04109f9c24387dc062577598839aaada5e47e56b
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-10-06 21:17:05 +0530

    pattern 7

---
+++ pattern18.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class pattern18 {
+        public static void main(String[] args) {
+            System.out.println("enter the value for which pattern is to be printed");
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int count;
+            for (int i = 1; i <= n; i++)
+            {
+                count=i;
+                for (int j = 1; j <= i; j++)
+                {
+    
+                    System.out.print(count +" ");
+                    count++;
+                }
+                System.out.println();
+            }
+        }
+    }    
