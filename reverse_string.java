73a560643f0d289ed5cfd235bc3537179fac44f0
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-11-23 20:47:05 +0530

    reverse string

---
+++ reverse_string.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+    public static void main(String[] args) {
+      String str= "ayush";
+      StringBuffer sb= new StringBuffer(str);
+      sb.reverse();
+      System.out.println(sb);
+    }
+}
